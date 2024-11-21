f20925d80ec3f6b2bea8c8eb01738da499c2987e
BorisZeBlade <romannesterenko@arizona.edu>
2024-11-21 12:55:24 +0500

    1

--- App.py
+++ App.py
@@ -3,41 +3,49 @@ from Pages import Home, Project1, Project2, Project3
 from streamlit_navigation_bar import st_navbar
 import os
 from PIL import Image
+import pandas as pd
+import numpy as np
+
 
 image = Image.open('img/barca.png')
 st.set_page_config(initial_sidebar_state="collapsed", page_icon=image)
 
 logo_path = os.path.join(os.path.dirname(__file__), "img", "barca.svg")
-pages = ['Home','Project1', 'Project2', 'Project3']
+pages = [" ",'Home','Project1', 'Project2', 'Project3']
 
 styles = {
     "nav": {
-        "background-color": "royalblue",
+        "background-color": "green",
         "display": "flex",
-        "justify-content": "center",
-        "position": "relative"
+        "justify-content": "center"
     },
     "img": {
         "position": "absolute",
-        "left": "10px",
+        "left": "-20px",
+        "font-size": "15px",
         "top": "4px",
-        "width": "150px",
+        "width": "100px",
         "height": "40px",
     },
     "span": {
+        "display": "block",
         "color": "white",
-        "padding": "0.4375rem 0.625rem",
+        "padding": "0.2rem 0.725rem",
+        "font-size": "14px"
     },
+
     "active": {
         "background-color": "white",
-        "color": "var(--text-color)",
+        "color": "black",
         "font-weight": "normal",
         "padding": "14px",
+
     }
 }
+
 options = {
     "show_menu": False,
-    "show_sidebar": False,
+    "show_sidebar": True,
 }
 
 page = st_navbar(pages,
@@ -55,7 +63,5 @@ elif page == "Project3":
     Project3.Project3().app()
 else:
     Home.Home().app()
-
-
 
 
--- Pages/Home.py
+++ Pages/Home.py
@@ -5,3 +5,4 @@ class Home:
         pass
     def app(self):
         st.write('Home')
+
